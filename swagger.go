f0ebd53d81a575ecb074bf1107b819e193e4593b
toni dy <tonidy@users.noreply.github.com>
2023-10-29 17:03:42 +0700

    Fix getForwardedPrefix function

--- swagger.go
+++ swagger.go
@@ -78,13 +78,14 @@ func New(config ...Config) fiber.Handler {
 func getForwardedPrefix(c *fiber.Ctx) string {
 	header := c.GetReqHeaders()["X-Forwarded-Prefix"]
 
-	if len(header) == 0 {
+	if header == "" {
 		return ""
 	}
 
 	prefix := ""
 
-	for _, rawPrefix := range header {
+	prefixes := strings.Split(header, ",")
+	for _, rawPrefix := range prefixes {
 		endIndex := len(rawPrefix)
 		for endIndex > 1 && rawPrefix[endIndex-1] == '/' {
 			endIndex--
